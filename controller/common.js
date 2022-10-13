49cc94463238d9976b44b7eaffdb5e2c2dc1b858
Dayal Mukati <dayalmukati@gmail.com>
2022-10-13 07:21:29 +0000

    outage apis completed

--- controller/common.js
+++ controller/common.js
@@ -5,7 +5,7 @@ exports.postLosscharge = async (req, res, next) => {
     try {
       req.body.id = new ObjectID().toHexString();
       const id = req.body.id;
-      req.body.docType = req.params.entity;
+      req.body.docType = req.params.entity + "losscharge";
       const lossData = await Gateway.evaluateTransaction(
         req.params.org,
         req.params.appUserId,
@@ -69,7 +69,7 @@ exports.getLosscharge = async (req, res, next) => {
         req.params.chaincodeName,
         "Find",
         JSON.stringify(obj),
-        req.params.entity
+        req.params.entity + "losscharge"
       );
       if (!loss) {
         res.status(400).json({
@@ -80,6 +80,92 @@ exports.getLosscharge = async (req, res, next) => {
         res.status(200).json({
           success: true,
           losses: loss,
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
+
+exports.postOutage = async (req, res, next) => {
+    try {
+      req.body.id = new ObjectID().toHexString();
+      const id = req.body.id;
+      req.body.docType = req.params.entity + "outage";
+      const outageData = await Gateway.evaluateTransaction(
+        req.params.org,
+        req.params.appUserId,
+        req.params.channelName,
+        req.params.chaincodeName,
+        "Find",
+        JSON.stringify({
+          Name: req.body.Name,
+        }),
+        req.params.entity
+      );
+      if (outageData[0]) {
+        await Gateway.submitTransaction(
+          req.params.org,
+          req.params.appUserId,
+          req.params.channelName,
+          req.params.chaincodeName,
+          "CreateData",
+          JSON.stringify(req.body)
+        );
+        const savedOutage = await Gateway.evaluateTransaction(
+          req.params.org,
+          req.params.appUserId,
+          req.params.channelName,
+          req.params.chaincodeName,
+          "Find",
+          JSON.stringify({
+            id: id,
+          }),
+          req.body.docType
+        );
+  
+        res.status(201).json({
+          success: true,
+          savedOutage: savedOutage[0],
+        });
+      } else {
+        res.status(400).json({
+          success: false,
+          meg: "Record Not Found",
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
+
+exports.getOutage = async (req, res, next) => {
+    try {
+      let obj = req.body;
+      for (var propName in obj) {
+        if (obj[propName] === null || obj[propName] === "") {
+          delete obj[propName];
+        }
+      }
+      console.log(obj, "req.body");
+      const outage = await Gateway.evaluateTransaction(
+        req.params.org,
+        req.params.appUserId,
+        req.params.channelName,
+        req.params.chaincodeName,
+        "Find",
+        JSON.stringify(obj),
+        req.params.entity + "outage"
+      );
+      if (!outage) {
+        res.status(400).json({
+          success: false,
+          msg: "No Record Found",
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          outages: outage,
         });
       }
     } catch (error) {
--- routes/common.js
+++ routes/common.js
@@ -1,12 +1,15 @@
 const {
    postLosscharge,
-   getLosscharge
+   getLosscharge,
+   postOutage,
+   getOutage
 } = require('../controller/common.js')
 
 const router = require('express').Router()
 //standard get & post
-router.post('/losscharge/:entity/:appuserId/:org/:channelName/:chaincodeName', postLosscharge)
-router.get('losscharge/:entity/:appuserId/:org/:channelName/:chaincodeName', getLosscharge)
+router.post('/losscharge/:entity/:appUserId/:org/:channelName/:chaincodeName', postLosscharge)
+router.get('/losscharge/:entity/:appUserId/:org/:channelName/:chaincodeName', getLosscharge)
 
-
+router.post('/outage/:entity/:appUserId/:org/:channelName/:chaincodeName', postOutage)
+router.get('/outage/:entity/:appUserId/:org/:channelName/:chaincodeName', getOutage)
 module.exports = router
